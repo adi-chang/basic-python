e318c32fce1f2481bde975d9926ccf41847b988c
adi-chang <adi4vista@gmail.com>
2020-11-07 15:38:34 +0700

    demo program pertemuan 01

---
+++ pertemuan_01.py
@@ -0,0 +1,171 @@
+# print 'Hello World'
+
+print('Hello World')
+
+
+# variable
+
+salam = 'selamat siang'
+print(salam)
+
+
+salam_ = 'Halo!'
+print(salam_)
+
+
+print('Budi', 123, '.')
+print('Budi', 123, '.', sep='')
+print('Budi', 123, '.', sep='-')
+
+
+print('Halo selamat pagi!', end='#')
+print('Saya ingin menjadi AI Profesional!')
+
+
+# ini untuk sapaan
+
+menyapa = 'Halo!'
+print(menyapa)
+
+
+# tipe data
+
+integer = 100
+print(integer)
+
+
+float = 3.6
+print(float)
+
+
+comp = 1+3j
+print(comp)
+
+
+print(type(integer))
+print(type(float))
+print(type(comp))
+
+
+string = "Indonesia"
+print(string)
+print(type(string))
+
+
+boolean = True
+print(boolean)
+print(type(boolean))
+
+
+num = 0
+print(bool(num))
+num = 1
+print(bool(num))
+
+
+# operasi dasar
+num_satu = 20
+num_dua = 30
+number = num_satu + num_dua
+print(number)
+
+
+num_satu = '20'
+num_dua = '30'
+number = num_satu + num_dua
+print(number)
+
+
+x = 20
+y = 19
+pengurangan = x - y
+print(pengurangan)
+
+
+x = 19
+y = 18
+kali = x * y
+print(kali)
+
+
+x_satu = 'satu'
+print(x_satu * 5)
+
+
+x =  15
+y = 2
+hasil = x / y
+print(hasil)
+
+
+x =  15
+y = 2
+hasil = x // y
+print(hasil)
+
+
+x =  16
+y = 3
+modulus = x % y
+print(modulus)
+
+
+x = 2
+y = 3
+pangkat = x ** y 
+print(pangkat)
+
+
+# operator penugasan dan short-hand operator
+
+x = 'Halo'
+print(x)
+
+
+x = 4
+x += 5
+print(x)
+
+
+
+x = 5
+x -= 4
+print(x)
+
+
+x = 15
+x /= 2
+print(x)
+
+
+x = 15
+x *= 99
+print(x)
+
+
+# operator perbandingan
+
+x = 4
+y = 7
+hasil = x > y
+print(hasil)
+hasil = x < y
+print(hasil)
+hasil = x == y
+print(hasil)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
