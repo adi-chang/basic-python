2358bfc71a2322e9e6973068748f7d6f55bd737b
adi-chang <adi4vista@gmail.com>
2020-11-08 15:39:22 +0700

    Assignment #1 - Basic Python

---
+++ Tugas-1/Soal-1.py
@@ -0,0 +1,7 @@
+nama = input('nama anda ? ')
+umur = int(input('umur anda ? '))
+tinggi = float(input('tinggi badan anda ? '))
+print('Nama Saya {}, Umur Saya {} tahun dan tinggi saya {} cm'.format(nama, umur, tinggi))
+
+# alternatif jika pakai python > 3.6
+# print(f'Nama Saya {nama}, Umur Saya {umur} tahun dan tinggi saya {tinggi} cm')
---
+++ Tugas-1/Soal-2.py
@@ -0,0 +1,3 @@
+jari_jari = float(input('Nilai Jari - Jari Lingkaran ? '))
+luas_lingkaran = 22 / 7 * jari_jari * jari_jari;
+print('Luas Lingkaran dengan jari-jari {} cm adalah {:.2f} cm\u00b2'.format(jari_jari, luas_lingkaran))
