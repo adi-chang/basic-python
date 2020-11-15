1cc58b68ba8a9416be0df98138667d9b7d746fab
adi-chang <adi4vista@gmail.com>
2020-11-15 15:05:41 +0700

    demo pertemuan-04

---
+++ basic-module/index.py
@@ -0,0 +1,9 @@
+# import module
+
+# import suhu
+from suhu import to_fahreinheit, to_celcius
+
+
+# memanggil fungsi
+to_celcius(212)
+to_fahreinheit(100)
---
+++ basic-module/suhu.py
@@ -0,0 +1,8 @@
+def to_celcius(fahreinheit):
+    cs = (fahreinheit - 32) * 5 / 9
+    print(cs)
+    
+    
+def to_fahreinheit(celcius):
+    fh = 9.0 / 5.0 * celcius + 32
+    print(fh)
